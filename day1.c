135ce2e15701d4705a5bc25b6e79777c5f5aa86f
Mike Perrow <mike.perrow@gmail.com>
2025-12-01 14:38:26 +0000

    day 1

---
+++ day1.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int parse_line(const char *line) {
+  int abs = atoi(line + 1);
+  return (line[0] == 'L') ? -abs : abs;
+}
+
+int make_move(int current_pos, int move) {
+  int new_pos = (current_pos + move) % 100;
+  if (new_pos < 0) {
+    new_pos += 100;
+  }
+  return new_pos;
+}
+
+int main() {
+  // char *input_path = "input/samples/day1_1.txt";
+  char *input_path = "input/real/day1_1.txt";
+
+  FILE *f = fopen(input_path, "r");
+  if (!f) {
+    perror("Failed to open file");
+    return EXIT_FAILURE;
+  }
+
+  int pos = 50;
+  int part1_count = 0;
+  int part2_count = 0;
+
+  char line[256];
+  while (fgets(line, sizeof(line), f)) {
+    int move = parse_line(line);
+    int small_move = move % 100;
+    int new_pos = make_move(pos, move);
+    part2_count += abs((int)(move / 100));
+
+    if ((pos > 0 && pos + small_move < 0) || pos < 0 && pos + small_move > 0) {
+      part2_count++;
+    }
+    if (pos + small_move > 100) {
+      part2_count++;
+    }
+    pos = new_pos;
+    if (pos == 0) {
+      part1_count++;
+      part2_count++;
+    }
+  }
+
+  printf("Part 1: %d\n", part1_count); // 964
+  printf("Part 2: %d\n", part2_count); // 5872
+
+  fclose(f);
+  return EXIT_SUCCESS;
+}
