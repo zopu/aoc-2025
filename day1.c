f6736df5d12802c06ad77df674a4821ee13fd449
Mike Perrow <mike.perrow@gmail.com>
2025-12-01 18:28:42 +0000

    day1: cleanup

--- day1.c
+++ day1.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define DIAL_SIZE 100
+#define DIAL_START 50
+
 int parse_line(const char *line) {
-  int abs = atoi(line + 1);
-  return (line[0] == 'L') ? -abs : abs;
+  int magnitude = atoi(line + 1);
+  return (line[0] == 'L') ? -magnitude : magnitude;
 }
 
 int make_move(int current_pos, int move) {
-  int new_pos = (current_pos + move) % 100;
+  int new_pos = (current_pos + move) % DIAL_SIZE;
   if (new_pos < 0) {
-    new_pos += 100;
+    new_pos += DIAL_SIZE;
   }
   return new_pos;
 }
 
 int main() {
-  // char *input_path = "input/samples/day1_1.txt";
-  char *input_path = "input/real/day1_1.txt";
+  // const char *input_path = "input/samples/day1_1.txt";
+  const char *input_path = "input/real/day1_1.txt";
 
   FILE *f = fopen(input_path, "r");
   if (!f) {
@@ -24,21 +27,22 @@ int main() {
     return EXIT_FAILURE;
   }
 
-  int pos = 50;
+  int pos = DIAL_START;
   int part1_count = 0;
   int part2_count = 0;
 
   char line[256];
   while (fgets(line, sizeof(line), f)) {
     int move = parse_line(line);
-    int small_move = move % 100;
+    int small_move = move % DIAL_SIZE;
     int new_pos = make_move(pos, move);
-    part2_count += abs((int)(move / 100));
+    part2_count += abs(move / DIAL_SIZE);
 
-    if ((pos > 0 && pos + small_move < 0) || pos < 0 && pos + small_move > 0) {
+    if ((pos > 0 && pos + small_move < 0) ||
+        (pos < 0 && pos + small_move > 0)) {
       part2_count++;
     }
-    if (pos + small_move > 100) {
+    if (pos + small_move > DIAL_SIZE) {
       part2_count++;
     }
     pos = new_pos;
